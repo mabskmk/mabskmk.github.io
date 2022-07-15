0bd29d212e0c2b98b7fb073ee912cf9c9648857c
mabskmk <mabskmk@gmail.com>
2022-07-15 18:12:18 -0300

    Update function_char.js

--- function_char.js
+++ function_char.js
@@ -114,7 +114,7 @@ case 27:
 mydiv='GnG-Graveyard',imgur='p19yaAN',imgur9='lIHIoKJ.png',game='Makaimura',ctype='Plataform',lastupdate='01/10/11',gdrv='1OlrmXy18dRVI1cL7VT8ocLGTvWbcp25h',youtube='yAXctj9pVZ4'
 break;
 case 28:
-mydiv='GnG-Tower',imgur='CkHRPvY',game='Makaimura',ctype='Arcade',lastupdate='30/09/11',gdrv='1s4SVGZOxGCeUxin621GOhFwMvfnIka-8',youtube='AgyJvl_cA-U'
+mydiv='GnG-Tower',imgur='CkHRPvY',imgur9='fH7y5Ge.png',game='Makaimura',ctype='Arcade',lastupdate='30/09/11',gdrv='1s4SVGZOxGCeUxin621GOhFwMvfnIka-8',youtube='AgyJvl_cA-U'
 break;
 case 29:
 mydiv='GnG-Burning',imgur='ScexylS',imgur9='ohZWqMw.png',game='Makaimura',ctype='SHMUP',lastupdate='29/09/11',gdrv='1rPMAP0HkLOvngIqFxxgNWjZYKKVt0IwB',youtube='oeOOCOa3QWI'
