9a12447d0c8c05507a93ede1c73cabff13079c31
mabskmk <mabskmk@gmail.com>
2022-11-14 17:56:50 -0300

    Update function_oshiro.js

--- function_oshiro.js
+++ function_oshiro.js
@@ -18,10 +18,10 @@ rand=Math.floor(Math.random() * 999);
 //rand=Math.floor((Math.random() * 10) + 999);
 oshiro(rand,3);
 if (special!=''){
-document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/"+special+"?autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br>Castle "+rand+": <b><font color=blue>"+mydiv+"</font></b>"
+document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/"+special+"?autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br>CASTLE ("+rand+"): <b><font color=blue>"+mydiv+"</font></b>"
 }
 else {
-vioshiro();
+vioshiro();//while
 }
 tempo=setTimeout('vichar()',50000);
 }
