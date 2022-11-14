415e1258fd41ffb0a82685ddab6b95ac8493ddb9
mabskmk <mabskmk@gmail.com>
2022-11-14 16:57:44 -0300

    Update function_oshiro.js

--- function_oshiro.js
+++ function_oshiro.js
@@ -1014,7 +1014,7 @@ document.getElementById(numero).innerHTML=""
 break;
 case 2:
 if (mydiv!=''&&special!=''&&destroyed!=''){
-document.getElementById(numero).innerHTML="<br><br><b>"+mydiv+"</b><br><iframe src=https://www.youtube.com/embed/"+special+" width='430' height='240' frameborder='0'></iframe><iframe src=https://www.youtube.com/embed/"+destroyed+" width='430' height='240' frameborder='0'></iframe><br><br>"
+document.getElementById(numero).innerHTML="<br><br><b>"+mydiv+"</b><br><iframe src=https://www.youtube.com/embed/"+special+" width='640' height='360' frameborder='0'></iframe><iframe src=https://www.youtube.com/embed/"+destroyed+" width='640' height='360' frameborder='0'></iframe><br><br>"
 }
 if (mydiv!=''&&special!=''&&destroyed==''){
 document.getElementById(numero).innerHTML="<br><br><b><font color=blue>"+mydiv+"</font></b><br><iframe src=https://www.youtube.com/embed/"+special+" width='640' height='360' frameborder='0'></iframe><br><br>"
