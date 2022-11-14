4a52b32a334db0981ec3035ad78146861407ca2c
mabskmk <mabskmk@gmail.com>
2022-11-14 16:52:09 -0300

    Update function_oshiro.js

--- function_oshiro.js
+++ function_oshiro.js
@@ -1017,10 +1017,10 @@ if (mydiv!=''&&special!=''&&destroyed!=''){
 document.getElementById(numero).innerHTML="<br><br><b>"+mydiv+"</b><br><iframe src=https://www.youtube.com/embed/"+special+" width='430' height='240' frameborder='0'></iframe><iframe src=https://www.youtube.com/embed/"+destroyed+" width='430' height='240' frameborder='0'></iframe><br><br>"
 }
 if (mydiv!=''&&special!=''&&destroyed==''){
-document.getElementById(numero).innerHTML="<br><br><b><font color=blue>"+mydiv+"</font></b><br><iframe src=https://www.youtube.com/embed/"+special+" width='430' height='480' frameborder='0'></iframe><br><br>"
+document.getElementById(numero).innerHTML="<br><br><b><font color=blue>"+mydiv+"</font></b><br><iframe src=https://www.youtube.com/embed/"+special+" width='640' height='360' frameborder='0'></iframe><br><br>"
 }
 if (mydiv!=''&&special==''&&destroyed!=''){
-document.getElementById(numero).innerHTML="<br><br><b><font color=yellow>"+mydiv+"</font></b><br><iframe src=https://www.youtube.com/embed/"+destroyed+" width='640' height='240' frameborder='0'></iframe><br><br>"
+document.getElementById(numero).innerHTML="<br><br><b><font color=yellow>"+mydiv+"</font></b><br><iframe src=https://www.youtube.com/embed/"+destroyed+" width='640' height='360' frameborder='0'></iframe><br><br>"
 }
 break;
 }
