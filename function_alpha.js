65b1c5955da725532aaf821e99dd4bd60127033b
mabskmk <mabskmk@gmail.com>
2023-07-23 16:38:04 -0300

    Update function_alpha.js
    Wip page completed 😅

--- function_alpha.js
+++ function_alpha.js
@@ -51,7 +51,7 @@ btntuto.disabled=false;
 btnyotu.disabled=true;
 break;
 case 6:
-document.getElementById('principal').innerHTML="<center><h1 id=nomeprinc><b>W.I.P.</h1><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><br><img src='https://i.imgur.com/E1IwRRY.png' width=12.5% height=12.5%><img src='https://i.imgur.com/E1IwRRY.png' width=12.5% height=12.5%><img src='https://i.imgur.com/E1IwRRY.png' width=12.5% height=25%><br><img src='https://i.imgur.com/RakduKl.png' width=25% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><br><br></center>"
+document.getElementById('principal').innerHTML="<center><h1 id=nomeprinc><b>W.I.P.</h1><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><br><img src='https://i.imgur.com/E1IwRRY.png' width=12.5% height=12.5%><img src='https://i.imgur.com/E1IwRRY.png' width=12.5% height=12.5%><img src='https://i.imgur.com/E1IwRRY.png' width=12.5% height=25%><br><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><img src='https://i.imgur.com/RakduKl.png' width=12.5% height=12.5%><br><br></center>"
 btnnew.disabled=false;
 btnchar.disabled=false;
 btnstg.disabled=false;
