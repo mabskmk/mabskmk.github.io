8c20164135a34fe217c4fd365b305667839024d0
mabskmk <mabskmk@gmail.com>
2023-09-15 15:07:22 -0300

    Update function_misc.js

--- function_misc.js
+++ function_misc.js
@@ -1,30 +1,7 @@
-//MENU-Misc ..atualizado 07/2023.
-function menumisc() {
-document.getElementById('principal').innerHTML="<table border=0><td align=center width='500'><h1 id=nomeprinc><b>Misc...</b></h1><b id=5></b><b id=4></b><b id=3></b><b id=2></b><b id=1></b><br><br><input type=button onClick='ppmain(1)' value='.Clean.'><br><tr></table>"
-btnnew.disabled=false;
-btnchar.disabled=false;
-btnstg.disabled=false;
-btnfullg.disabled=false;
-btnyotu.disabled=false;
-}
-
 function viytmisc() {
 rand=Math.floor(Math.random() * 6);
-//rand=Math.floor((Math.random() * 10) + 999);
-ytmisc(rand);
-if (youtube!=''){
-document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/"+youtube+"autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br><b>"+mydiv+"</b>"
-}
-else {
-document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/b7vz7krharY?autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br><b>Just a normal day</b>"
-}
-tempo=setTimeout('vichar()',50000);
-}
-
-//YOUTUBE VIODES
-function ytMisc(numero,cod) {
 mydiv='';youtube='';
-switch (numero) {
+switch (rand) {
 case 1:
 mydiv='MUGEN_-_Fantastico_Jaspion',youtube='1_7gepKMuts?'
 break;
@@ -40,8 +17,12 @@ break;
 case 5:
 mydiv='MUGEN_-_Dr.Wily',special='uNyQi6YTfg8?t=18&'
 break;
-default:
-mydiv=''
-break;
 }
+if (youtube!=''){
+document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/"+youtube+"autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br><b>"+mydiv+"</b>"
+}
+else {
+document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/b7vz7krharY?autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br><b>Just a normal day</b>"
+}
+tempo=setTimeout('vichar()',50000);
 }
