346d9f07042277b83eb618d4b1d5281bd4377056
mabskmk <mabskmk@gmail.com>
2023-09-15 16:01:29 -0300

    Update function_misc.js
    staaaaart

--- function_misc.js
+++ function_misc.js
@@ -1,6 +1,6 @@
 function viytmisc() {
 rand=Math.floor(Math.random() * 6);
-mydiv='';youtube='';tea='';
+mydiv='';youtube='';start='';
 switch (rand) {
 case 1:
 mydiv='MUGEN_-_Fantastico_Jaspion',youtube='1_7gepKMuts'
@@ -9,17 +9,17 @@ case 2:
 mydiv='Alex_Kidd_in_Trouble_-_The_Hand_clan',youtube='1JUxenU5Ct8'
 break;
 case 3:
-mydiv='MAGMML2_-_Chomp_man',youtube='0IACU-xea7o',tea='&t=737s'
+mydiv='MAGMML2_-_Chomp_man',youtube='0IACU-xea7o',start='&t=737'
 break;
 case 4:
-mydiv='Nemesis_Online_-_Crystal_Forest',youtube='vsisup-v2mk',tea='&t=101'
+mydiv='Nemesis_Online_-_Crystal_Forest',youtube='vsisup-v2mk',start='&t=101'
 break;
 case 5:
-mydiv='MUGEN_-_Dr.Wily',special='uNyQi6YTfg8',tea='&t=18'
+mydiv='MUGEN_-_Dr.Wily',special='uNyQi6YTfg8',start='&t=18'
 break;
 }
 if (youtube!=''){
-document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/"+youtube+"?autoplay=1&mute=1&t=40s width='500' height='300' frameborder='0'></iframe><br><b>"+mydiv+"</b>"
+document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/"+youtube+"?autoplay=1&mute=1"+start+" width='500' height='300' frameborder='0'></iframe><br><b>"+mydiv+"</b>"
 }
 else {
 document.getElementById('videoshowcase').innerHTML="<iframe src='https://www.youtube.com/embed/b7vz7krharY?autoplay=1&mute=1' width='500' height='300' frameborder='0'></iframe><br><b>Just a normal day</b>"
