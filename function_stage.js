8caf177630651db47dc0fceb4905f559e5baccd8
mabskmk <mabskmk@gmail.com>
2023-12-24 17:08:20 -0300

    Update function_stage.js

--- function_stage.js
+++ function_stage.js
@@ -34,9 +34,7 @@ document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.yout
 else{
 document.getElementById('videoshowcase').innerHTML="<iframe src=https://www.youtube.com/embed/HZxpB5C-iuA?autoplay=1&mute=1 width='500' height='300' frameborder='0'></iframe><br>Full-Game: <b>Alex Kidd In Trouble</b> <a target='_blank' href='https://www.dropbox.com/s/wueu530ny6iowdi/AlexKiddinTrouble.zip?dl=1'><img src=https://i.imgur.com/pJFCm25.png title='Alex'></a>"
 }
-if (bannerhtml!=''){
 document.getElementById('banner').innerHTML="<a target='_blank' src=https://"+bannerhtml+"><img scr=https://"+bannerimg+"></a>";
-}
 tempo=setTimeout('viytmisc()',50000);
 }
 
