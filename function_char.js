5c79e54a241e319b756d8486bc6ec28c1a751d26
mabskmk <mabskmk@gmail.com>
2024-01-04 01:57:50 -0300

    Update function_char.js

--- function_char.js
+++ function_char.js
@@ -16,8 +16,8 @@ char(27,2);
 }
 
 function vichar() {
-while (rand>54) do {
-rand=(rand-54)
+while (rand>54) {
+rand=(rand-54);
 }
 rand=Math.floor(Math.random() * (54-rand));
 //rand=Math.floor((Math.random() * 5) + 45);
