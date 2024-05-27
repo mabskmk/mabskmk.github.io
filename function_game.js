6b36fd31052b35e42c478be5575617c2540bf6fe
mabskmk <mabskmk@gmail.com>
2024-05-26 22:22:37 -0300

    Add files via upload
    Progress

--- function_game.js
+++ function_game.js
@@ -5,22 +5,153 @@ var playerOne = new Array(9);
 var playerTwo = new Array(9);
 var idSelect=0;
 var cardIMG=0;
+var card1Jog; var card2Jog; var card3Jog; var card4Jog; var card5Jog;
+var card1Cpu; var card2Cpu; var card3Cpu; var card4Cpu; var card5Cpu;
+
 
 //Creates the basic html
 function menugame() {
-document.getElementById('principal').innerHTML="<center><h1 id=nomeprinc><b>W.I.P.</h1><table border=0><tr><td align=center><img id=1 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=2 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=3 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center rowspan=3 bgcolor=darkolivegreen><img id=10 src='https://i.imgur.com/y3p2oz2.png'><td align=center rowspan=3><img id=11 src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=12 src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=13 src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=14 src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=15 src='https://i.imgur.com/EEQqMRF.png' height='80px'><tr><td align=center><img id=4 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=5 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=6 src='https://i.imgur.com/ezqUkPo.png' height='135px'><tr><td align=center><img id=7 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=8 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=9 src='https://i.imgur.com/ezqUkPo.png' height='135px'><tr></table>(<b id=v1></b>)(<b id=v2></b>)(<b id=v3></b>)(<b id=v4></b>)(<b id=v5></b>)(<b id=v6></b>)(<b id=v7></b>)(<b id=v8></b>)(<b id=v9></b>)</center>"
+document.getElementById('principal').innerHTML="<center><h1 id=nomeprinc><b>W.I.P.</h1><table border=0><tr><td align=center><img id=1 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=2 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=3 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center rowspan=3 bgcolor=darkolivegreen height=408><img id=10 src='https://i.imgur.com/y3p2oz2.png'><td align=center rowspan=3><img id=11 onClick='seLect(11)' src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=12 onClick='seLect(12)' src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=13 onClick='seLect(13)' src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=14 onClick='seLect(14)' src='https://i.imgur.com/EEQqMRF.png' height='80px'><br><img id=15 onClick='seLect(15)' src='https://i.imgur.com/EEQqMRF.png' height='80px'><tr><td align=center><img id=4 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=5 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=6 src='https://i.imgur.com/ezqUkPo.png' height='135px'><tr><td align=center><img id=7 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=8 src='https://i.imgur.com/ezqUkPo.png' height='135px'><td align=center><img id=9 src='https://i.imgur.com/ezqUkPo.png' height='135px'><tr></table>(<b id=v1></b>)(<b id=v2></b>)(<b id=v3></b>)(<b id=v4></b>)(<b id=v5></b>)(<b id=v6></b>)(<b id=v7></b>)(<b id=v8></b>)(<b id=v9></b>)<br><br></center>"
 btnnew.disabled=false;
 btnchar.disabled=false;
 btnstg.disabled=false;
 btnfullg.disabled=false;
 btnyotu.disabled=false;
-clearTable();
-cpuAI();
 startGame();
+}
+
+
+function startGame() {
+
+	//alert("Start Game");
+
+	clearTable();
+
+	//Players hand
+	card1Jog=drawCard();  
+	card2Jog=drawCard();  
+	card3Jog=drawCard();  
+	card4Jog=drawCard();  
+	card5Jog=drawCard();  
+
+	//Players frontend
+	document.getElementById(11).src = getImage(card1Jog,1);
+	document.getElementById(12).src = getImage(card2Jog,1);
+	document.getElementById(13).src = getImage(card3Jog,1);
+	document.getElementById(14).src = getImage(card4Jog,1);
+	document.getElementById(15).src = getImage(card5Jog,1);
+	document.getElementById(11).style = 'opacity: 0.5;';
+	document.getElementById(12).style = 'opacity: 0.5;';
+	document.getElementById(13).style = 'opacity: 0.5;';
+	document.getElementById(14).style = 'opacity: 0.5;';
+	document.getElementById(15).style = 'opacity: 0.5;';
+
+	//Computers hand
+	card1Cpu=drawCard();  
+	card2Cpu=drawCard();  
+	card3Cpu=drawCard();  
+	card4Cpu=drawCard();  
+	card5Cpu=drawCard();  
+
+	computerTime();
+
+
+//Deck debug
+document.getElementById('v1').innerHTML=cardArray[0];
+document.getElementById('v2').innerHTML=cardArray[1];
+document.getElementById('v3').innerHTML=cardArray[2];
+document.getElementById('v4').innerHTML=cardArray[3];
+document.getElementById('v5').innerHTML=cardArray[4];
+document.getElementById('v6').innerHTML=cardArray[5];
+document.getElementById('v7').innerHTML=cardArray[6];
+document.getElementById('v8').innerHTML=cardArray[7];
+document.getElementById('v9').innerHTML=cardArray[8];
+}
+
+
+//Deck with 45 cards (1x'9', 2x'8', 3x'7', 4x'6', 5x'5', 6x'4', 7x'3', 8x'2' and 9x'1')
+//Possible values '0' to '8'
+
+function drawCard() {
+  rand=(Math.floor(Math.random()*9));
+  while (cardArray[rand]==0) {rand=(Math.floor(Math.random()*9));}
+  if (cardArray[rand]>0) {cardArray[rand]=cardArray[rand]-1;}
+  return(rand);
+}  
+
+
+//Show the highest card on the table
+
+function highCard() {
+
+cardIMG=-1; //the value of zero is used for the card number 1
+idSelect=0;
+
+	//Compares both arrays
+	//idSelect gets the player side
+	for (x=0;x<9;x++) {
+		if ((playerOne[x]>0) && (playerOne[x]>cardIMG)) {cardIMG = playerOne[x]; idSelect=1;}
+	}
+	for (x=0;x<9;x++) {
+		if ((playerTwo[x]>0) && (playerTwo[x]>cardIMG)) {cardIMG = playerTwo[x]; idSelect=2;}
+	}
+
+	document.getElementById(10).src = getImage(cardIMG,idSelect);
+return;
+}
+
+
+//Simple version only to show the cards on the table
+
+function computerTime() {
+
+	rand=(Math.floor(Math.random()*5));
+	switch (rand) {
+		case 0:
+  		idSelect=card1Cpu;
+		break;
+		case 1:
+  		idSelect=card2Cpu;
+		break;
+		case 2:
+  		idSelect=card3Cpu;
+		break;
+		case 3:
+  		idSelect=card4Cpu;
+		break;
+		case 4:
+  		idSelect=card5Cpu;
+		break;
+	}
+	rand=(Math.floor(Math.random()*9));
+	playerTwo[rand]=idSelect;
+
+	//html indexes start with 1
+ 	rand=rand+1;
+
+	document.getElementById(rand).src = getImage(idSelect,2);
+	highCard();
+	//alert("Computador escolheu campo :"+rand);
+}
+
+
+//Card "serecto"
+function seLect(idSelect) {
+
+	document.getElementById(11).style = 'opacity: 0.5;';
+	document.getElementById(12).style = 'opacity: 0.5;';
+	document.getElementById(13).style = 'opacity: 0.5;';
+	document.getElementById(14).style = 'opacity: 0.5;';
+	document.getElementById(15).style = 'opacity: 0.5;';
+
+	document.getElementById(idSelect).style = '';//magic
+
+
 // style = 'border:4px dashed green;' 
 }
 
-//Clear everything
+//Resets everything!!
+
 function clearTable() {
 document.getElementById('1').src="https://i.imgur.com/ezqUkPo.png";
 document.getElementById('2').src="https://i.imgur.com/ezqUkPo.png";
@@ -47,55 +178,19 @@ cardArray[5]=4;
 cardArray[6]=3;
 cardArray[7]=2;
 cardArray[8]=1;
-playerOne = [];
-playerTwo = [];
+for (x=0;x<10;x++) {
+	playerOne[x] = -1; //0 or -1?
+	playerTwo[x] = -1; //0 or -1?
 }
-
-//Bugged
-function startGame() {
-for (x=0;x<6;x++) {
-rand=(Math.floor(Math.random()*9));
-while (cardArray[rand]==0) {rand=(Math.floor(Math.random()*9));}
-if (cardArray[rand]>0) {cardArray[rand]=cardArray[rand]-1;}
-cardIMG=getImage(rand,1);
-idSelect=11+x;
-document.getElementById(idSelect).src=cardIMG;
-}
+return;
 }
 
 
-//Cpu
-function cpuAI() {
 
-  //Set a randomValue
-  rand=(Math.floor(Math.random()*9));
+//Get an url for the actual card
 
-  //Compares with the card box
-  while (cardArray[rand]==0) {rand=(Math.floor(Math.random()*9));}
-
-  if (cardArray[rand]>0) {cardArray[rand]=cardArray[rand]-1;}
-
-//  alert(rand+1);
-  cardIMG=getImage(rand+1,2);
-  rand=(Math.floor(Math.random()*9));
-  document.getElementById(rand).src=cardIMG;
-  document.getElementById(10).src=cardIMG;
-
-//debug
-document.getElementById('v1').innerHTML=cardArray[0];
-document.getElementById('v2').innerHTML=cardArray[1];
-document.getElementById('v3').innerHTML=cardArray[2];
-document.getElementById('v4').innerHTML=cardArray[3];
-document.getElementById('v5').innerHTML=cardArray[4];
-document.getElementById('v6').innerHTML=cardArray[5];
-document.getElementById('v7').innerHTML=cardArray[6];
-document.getElementById('v8').innerHTML=cardArray[7];
-document.getElementById('v9').innerHTML=cardArray[8];
-}
-
-//Card database
 function getImage(card,player) {
-if (player==2) {card=card+8}
+if (player==2) {card=card+9}
 
 switch (card) {
 case 0:
