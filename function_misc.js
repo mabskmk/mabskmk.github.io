23846e3bac634f4fda5ddc2a16da9a89758add31
mabskmk <mabskmk@gmail.com>
2024-09-24 18:58:18 -0300

    Update function_misc.js

--- function_misc.js
+++ function_misc.js
@@ -51,7 +51,7 @@ case 7:
 mydiv='Today is the day to test your limits',youtube='pxmD8ycQgzM?si=ewnp4Eb3-y3mEy5G&start=50&end=200'
 break;
 case 8:
-mydiv='Sorry the purists... this is MSW',youtube='cNc87Fx8r-4?si=iUH1GVvbBl_tjEv0&start=150&end=300'
+mydiv='Spoilers!!!',youtube='GYEI39sPxJY&start=145&end=205'
 break;
 case 9:
 mydiv='Check out this crazy compilation',youtube='h5k5G6Dt6ho?si=26jFNX_e327bRdwd&start=703&end=853',link='drive.google.com/drive/folders/1a5la0Q-hWKLvAitgPxGrr6br_0lJ7kZb?usp=drive_link'
