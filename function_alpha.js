01cb85612d6beca8785aee44cf521b38f9542540
mabskmk <mabskmk@gmail.com>
2026-09-03 18:00:21 -0300

    Update function_alpha.js
    isVertical

--- function_alpha.js
+++ function_alpha.js
@@ -2,6 +2,7 @@ var playerOne = new Array(16);	//Player table, cards and hand
 var intervalTime;
 const hoje = new Date();
 const isHalloween = ((hoje.getDate()>=13 && hoje.getDate()<=31 && hoje.getMonth()==9) || (hoje.getDate()<3 && hoje.getMonth()==10) || (hoje.getDay()==5 && hoje.getDate()==13) ? 1: 0);
+var isVertical = false;
 
 // "Gambiarra"
 function buildTemplate() {
@@ -16,6 +17,7 @@ if (element1.offsetWidth == element2.offsetWidth) {
 document.getElementById('sitetemplate').innerHTML="<table border=2 bgcolor='black' id='tableid'><td align=center width='600'><img src='https://i.imgur.com/BsgTLng.png'/><h1>MABS PSEUDO SPACE</h1><img class='flip' id='BPt' src='https://signavatar.com/2425_v.gif'/><tr><td align=center valign=top><div id='cabec'></div><div id='principal'></div><img src=https://i.imgur.com/a13frey.gif><b id='banner'></b><tr><td align=center valign=top width=150 ><a target='_blank' href='https://s05.flagcounter.com/count/az53/bg_0C160D/txt_F2F2F2/border_008001/columns_2/maxflags_10/viewers_0/labels_0/pageviews_1/flags_1/'><img src='https://imgur.com/LssVM0G.png' alt='Free counters!' border='0'/></a><a target='_blank' href='https://s05.flagcounter.com/more/az53'></a><br><span id='linkiss'></span><tr></table>"
 document.getElementById('cabec').innerHTML="<b><a href='https://en.wikipedia.org/wiki/M.U.G.E.N' target='_blank' id='top'><u>M.U.G.E.N</u></a></b><br>A 2D fighting game engine that enables you to create fighting games.<br>Almost everything can be customized, from individual characters to stages, as well as the look and feel of the game.</b><br>I hope you enjoy my 'work'<br><br><input type=button onClick='menuchar()' value='Characters' id='btnchar'><input type=button onClick='menustage1()' value='Stages' id='btnstg'><input type=button onClick='menufullgame()' value='Fullgames' id='btnfullg'><input type=button onClick='menugame()' value='Battle!' id='btnyotu'><input type=button onClick='ppmain(2)' value='Showcase' id='btnnew'><br>"
 document.getElementById('principal').innerHTML="<center><table border=0><td align=center><br><div id=videoshowcase><iframe src='https://www.youtube.com/embed/videoseries?list=PLwmmiI53xsNQ_RocWwXBzX05hbsZE3gh5' width='430' height='240' frameborder='0'></iframe></div><tr></table></center>"
+isVertical = true;
 }
   ppmain(2);
 }
